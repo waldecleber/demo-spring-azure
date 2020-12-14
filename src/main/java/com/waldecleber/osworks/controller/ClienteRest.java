04106724fbbfe68efa2c627d3d961df29bf8a39d
Waldecleber Oliveira Gonçalves <waldecleber.g@e-unicred.com.br>
2020-12-14 16:41:54 -0300

    Adiciona Postgres

--- src/main/java/com/waldecleber/osworks/controller/ClienteRest.java
+++ src/main/java/com/waldecleber/osworks/controller/ClienteRest.java
@@ -1,16 +1,26 @@
 package com.waldecleber.osworks.controller;
 
-import com.waldecleber.osworks.dto.ClienteDTO;
-import com.waldecleber.osworks.service.ClienteService;
+import java.util.List;
+import java.util.Objects;
+
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.waldecleber.osworks.dto.ClienteDTO;
+import com.waldecleber.osworks.model.Cliente;
+import com.waldecleber.osworks.service.ClienteService;
 
 @RestController
 @RequestMapping("/clientes")
@@ -29,4 +39,25 @@ public class ClienteRest {
 	public List<ClienteDTO> listarClientes() {			
 		return clienteService.listarClientes();
 	}
+	
+	@GetMapping(value = "/{cpf}")	
+	public ResponseEntity<ClienteDTO> buscarClientePorCpf(@PathVariable String cpf) {			
+		ClienteDTO cliente = clienteService.buscarClientePorCpf(cpf);
+		if (Objects.nonNull(cliente)) {
+			return ResponseEntity.ok().body(cliente);
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ClienteDTO salvar(@RequestBody ClienteDTO dto) {
+		return clienteService.salvar(dto);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<ClienteDTO> atualizar(@PathVariable Long id, @RequestBody ClienteDTO cliente) {
+		return ResponseEntity.ok(clienteService.atualizar(id, cliente));
+	}
+	
 }
--- src/main/java/com/waldecleber/osworks/service/ClienteService.java
+++ src/main/java/com/waldecleber/osworks/service/ClienteService.java
@@ -1,18 +1,26 @@
 package com.waldecleber.osworks.service;
 
-import com.waldecleber.osworks.dto.ClienteDTO;
-import com.waldecleber.osworks.repository.ClienteRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
+import com.waldecleber.osworks.dto.ClienteDTO;
+import com.waldecleber.osworks.exception.ClienteDuplicadoException;
+import com.waldecleber.osworks.exception.ClienteNaoEncontrado;
+import com.waldecleber.osworks.model.Cliente;
+import com.waldecleber.osworks.repository.ClienteRepository;
 
 @Service
 public class ClienteService {
 	
+	
 	private final ClienteRepository clienteRepository;
+	
 	private final ModelMapper mapper;
 	
 	@Autowired
@@ -20,15 +28,43 @@ public class ClienteService {
 		this.clienteRepository = clienteRepository;
 		this.mapper = mapper;
 	}
+
+	public ClienteDTO salvar(ClienteDTO dto) {
+		Optional<Cliente> optional = clienteRepository.findByCpf(dto.getCpf());
+		if (optional.isPresent()) {
+			throw new ClienteDuplicadoException("Já existe um Cliente com este CPF");			
+		}
+		Cliente cliente = mapper.map(dto, Cliente.class);
+		clienteRepository.save(cliente);
+		return dto;
+	}
 	
 	public List<ClienteDTO> listarClientes() {
-		ClienteDTO cliente = ClienteDTO.builder()
-				.nome("Cliente 01")
-				.telefone("9999-0000")
-				.email("cliente@teste.com")
-				.cpf("17482812164")
-				.build();
-
-		return Collections.singletonList(cliente);
+		return clienteRepository.findAll().stream()
+			.map(cliente -> mapper.map(cliente, ClienteDTO.class))
+			.collect(Collectors.toList());		
 	}
+	
+	public ClienteDTO buscarClientePorNome(String nome) {
+		return clienteRepository.findByNome(nome)
+				.map(cliente -> mapper.map(cliente, ClienteDTO.class))
+				.orElseThrow(() -> new ClienteNaoEncontrado("Cliente não encontrado"));
+	}
+	
+	public ClienteDTO buscarClientePorCpf(String cpf) {
+		return clienteRepository.findByCpf(cpf)
+				.map(cliente -> mapper.map(cliente, ClienteDTO.class))
+				.orElseThrow(() -> new ClienteNaoEncontrado("Cliente não encontrado"));
+	}
+
+	public ClienteDTO atualizar(Long id, ClienteDTO dto) {
+		if (clienteRepository.existsById(id)) {
+			Cliente cliente = mapper.map(dto, Cliente.class);
+			cliente.setId(id);
+			return mapper.map(clienteRepository.save(cliente), ClienteDTO.class);
+		} else {
+			throw new ClienteNaoEncontrado("Cliente não encontrado");			
+		}
+	}
+
 }
